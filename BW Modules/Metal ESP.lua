42280868c997d59535fdbc8ea3e9045a5fdf7a2d
Vxalware <jainasinth@gmail.com>
2025-11-20 18:18:22 +0530

    forgot to add a comma 💀

--- BW Modules/Metal ESP.lua
+++ BW Modules/Metal ESP.lua
@@ -9,7 +9,7 @@ runcode(function()
     local MetalESP
 
     MetalESP = vape.Categories.Minigames:CreateModule({
-        Name = "MetalESP"
+        Name = "MetalESP",
         Function = function(call)
             if call then
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/void2realyt/RainWare-V6-Vxpe-Loader-Script/refs/heads/main/RainWare%20V6%20Metal%20Detector%20ESP",true))()
