94baca2c10bb674892fa1a1fb2daa74b608f37f3
Vxalware <jainasinth@gmail.com>
2025-11-20 16:33:39 +0530

    Disabled Fix

--- BW Modules/Disabler.lua
+++ BW Modules/Disabler.lua
@@ -2,14 +2,21 @@ runcode(function()
     local Disabler
 
     Disabler = vape.Categories.Minigames:CreateModule({
-        Name = "Disabler"
+        Name = "Disabler",
         Function = function(call)
             if call then
-                bedwars.AbilityController:useAbility("jade_hammer_jump")
-                JadeTP.ToggleButton(false)
-                game.Players.LocalPlayer.Character.Humanoid.Walkspeed = 50
-                wait(3)
-                game.Players.LocalPlayer.Character.Humanoid.Walkspeed = 23
+                pcall(function()
+                    bedwars.AbilityController:useAbility("jade_hammer_jump")
+                end)
+
+                local hum = game.Players.LocalPlayer.Character
+                    and game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+
+                if hum then
+                    hum.WalkSpeed = 50
+                    task.wait(3)
+                    hum.WalkSpeed = 23
+                end
             end
         end,
         HoverText = "Jade Disabler"
