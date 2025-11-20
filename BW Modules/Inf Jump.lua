ae3a7049b1d44e232a83963093975f4002761fda
Vxalware <jainasinth@gmail.com>
2025-11-20 17:05:51 +0530

    Update Inf Jump.lua

--- BW Modules/Inf Jump.lua
+++ BW Modules/Inf Jump.lua
@@ -1,1 +1,26 @@
-wf
+run(function()
+    local InfJump
+    local connection
+
+    InfJump = vape.Categories.Minigames:CreateModule({
+        Name = "InfJump",
+        Function = function(call)
+            if enabled then
+                connection = game:GetService("UserInputService").JumpRequest:Connect(function()
+                    local player = game:GetService("Players").LocalPlayer
+                    local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+                    if humanoid then
+                        humanoid.Jump = true
+                        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+                    end
+                end)
+            else
+                if connection then
+                    connection:Disconnect()
+                    connection = nil
+                end
+            end
+        end,
+        HoverText = "Jump Infinitely!"
+    })
+end)
