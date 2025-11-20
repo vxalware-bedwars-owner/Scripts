a2a4673c6a94264acd07b8022c08349b0a3e2ca0
Vxalware <jainasinth@gmail.com>
2025-11-20 16:07:13 +0530

    Update Metal ESP.lua

--- BW Modules/Metal ESP.lua
+++ BW Modules/Metal ESP.lua
@@ -1,1 +1,13 @@
-whrhwr
+runcode(function()
+    local MetalESP
+
+    MetalESP = vape.Categories.Minigames:CreateModule({
+        Name = "MetalESP"
+        Function = function(call)
+            if call then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/void2realyt/RainWare-V6-Vxpe-Loader-Script/refs/heads/main/RainWare%20V6%20Metal%20Detector%20ESP",true))()
+            end
+        end,
+        HoverText = "Metal Detector ESP"
+    })
+end)
