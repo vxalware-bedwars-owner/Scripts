1a3d9b2a36cc083619642131cdb00e2d74e7d9cd
Vxalware <jainasinth@gmail.com>
2025-11-20 17:30:40 +0530

    Update Disabler.lua

--- BW Modules/Disabler.lua
+++ BW Modules/Disabler.lua
@@ -13,17 +13,14 @@ runcode(function()
         Name = "Disabler",
         Function = function(call)
             if call then
-                -- Use Jade ability safely
                 pcall(function()
                     bedwars.AbilityController:useAbility("jade_hammer_jump")
                 end)
 
-                -- Kill old thread if exists
                 if speedThread then
                     task.cancel(speedThread)
                 end
 
-                -- Thread so disabling/uninjecting doesn’t break anything
                 speedThread = task.spawn(function()
                     local plr = game.Players.LocalPlayer
                     local hum = plr.Character and plr.Character:FindFirstChildOfClass("Humanoid")
@@ -31,21 +28,18 @@ runcode(function()
 
                     hum.WalkSpeed = 50
 
-                    -- Wait 3 seconds OR end early if module disabled / vape uninjects
                     local t = 0
                     while t < 3 and Disabler.Enabled and vape and vape.LOADED do
                         task.wait(0.05)
                         t += 0.05
                     end
 
-                    -- If still valid, restore speed
                     if hum and hum.Parent and Disabler.Enabled and vape and vape.LOADED then
                         hum.WalkSpeed = 23
                     end
                 end)
 
             else
-                -- On disable: cancel thread & restore WS safely
                 if speedThread then
                     task.cancel(speedThread)
                     speedThread = nil
