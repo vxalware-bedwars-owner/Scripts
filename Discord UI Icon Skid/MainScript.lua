9f40e3d3b6643baf0b4f1e8bc98bac87ecee4116
Vxalware <jainasinth@gmail.com>
2026-03-03 10:12:03 +0800

    Update MainScript.lua

--- Discord UI Icon Skid/MainScript.lua
+++ Discord UI Icon Skid/MainScript.lua
@@ -1,58 +1,62 @@
-local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))() -- loads Discord UI
-local Window = DiscordLib:Window("Discord UI Icon Skid from ScriptHub V3") -- Window
-local Server = Window:Server("Features", "http://www.roblox.com/asset/?id=6031075938") -- Server
+--> Discord UI Dependencies
+local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))()
+local Window = DiscordLib:Window("Discord UI Icon Skid from ScriptHub V3")
 
-local MainTab = Server:Channel("Main") -- Main Tab
+--> Primary & Secondary tabs
+local Server = Window:Server("Features", "http://www.roblox.com/asset/?id=6031075938")
+local MainTab = Server:Channel("Main")
+
+-- Button
 MainTab:Button("Button", function()
   print("Clicked button")
-end) -- Button
+end)
 
+-- Toggle
 MainTab:Toggle("Toggle", false, function(state)
 	if state then
 		print("executed")
 	else
 		print("unexecuted")
 	end
-end) -- Toggle
+end)
 
+-- Slider
 MainTab:Slider("Slider (WalkSpeed)", 16, 100, 21, function(value)
 	local Players = game:GetService("Players")
 	local player = Players.LocalPlayer
-	if not player then
-		warn("LocalPlayer not found")
-		return
-	end
-
 	local character = player.Character or player.CharacterAdded:Wait()
 	local humanoid = character:FindFirstChildOfClass("Humanoid")
 	if humanoid then
 		humanoid.WalkSpeed = value
 		print("WalkSpeed set to:", value)
-	else
-		warn("Humanoid not found")
 	end
-end) -- Slider
+end)
 
+-- Dropdown
 MainTab:Dropdown("Dropdown", { "None", "Example 1" }, function(option)
 	if option == "None" then
 		print("None")
 	elseif option == "Example 1" then
 		print("Example 1")
 	end
-end) -- Dropdown
+end)
 
+-- Colorpicker
 MainTab:Colorpicker("Colorpicker", Color3.fromRGB(255, 1, 1), function(color)
 	print("color: " .. tostring(color))
-end) -- Colorpicker
+end)
 
+-- Textbox
 MainTab:Textbox("Textbox", "Enter text...", true, function(input)
 	print("text entered: " .. input)
-end) -- Textbook
+end)
 
-local CreditsTab = Server:Channel("Credits") -- Credits Tab
+--> Credits tab & Label example
+local CreditsTab = Server:Channel("Credits")
 CreditsTab:Label("This example is made by SynthX. Script provided by dawidscripts (library author).") -- Labe;
 
+-- Notification
 CreditsTab:Button("Notification", function()
 	DiscordLib:Notification("Notification", "This is a notification", "Okay")
-end) -- Notification
+end)
 -- This is just an example. Official example with other features may be found at https://github.com/weakhoes/Roblox-UI-Libs/blob/main/Discord%20UI%20Lib/Discord%20Lib%20Example.lua
